765aacf911c1227df6ff89e7797b7467e3434eb1
ks406362 <k.sota@student.uw.edu.pl>
2019-11-30 18:38:05 +0100

    litera, fib

--- fibin.cpp
+++ fibin.cpp
@@ -9,23 +9,45 @@ struct Fib : std::integral_constant<int, Fib<n- 1>::value + Fib<n - 2>::value> {
 
 template<>
 struct Fib<1> : std::integral_constant<int, 1> {};
-
 template<>
 struct Fib<0> : std::integral_constant<int, 0> {};
 
+
 template<typename T>
 class Fibin{
-    
+    using type = T;
 };
 
 struct True { static constexpr bool value = true; };
 struct False { static constexpr bool value = false; };
 
-template<class T>
-struct Lit<T> ...;
+
+template<typename T>
+struct Lit {};
+
+template <int n>
+struct Lit<Fib<n>> : Fib<n>{};
 
 template<>
-struct Lit<True> : std::bool_constant<true> {};
+struct Lit<False> : std::false_type {} ;
 
 template<>
-struct Lit<False> : std::bool_constant<false> {};
+struct Lit<True> : std::true_type {};
+
+template<typename T, typename U>
+struct Eq {
+
+};
+
+template<typename T>
+struct Eq<T, T> {
+
+};
+
+
+int main(){
+    static_assert(2 == Lit<Fib<3>>());
+    return 0;
+}
+
+
--- fibin.h
+++ fibin.h
@@ -1,6 +1,4 @@
-//
-// Created by krzubuntu on 29.11.2019.
-//
+
 
 #ifndef JNP1_FIBIN_MASTER_FIBIN_H
 #define JNP1_FIBIN_MASTER_FIBIN_H
